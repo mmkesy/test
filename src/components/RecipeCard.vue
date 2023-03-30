1ec7890629df0595a9cde5dd94bfd9f9b2479641
Maciej Kęsy <admin@Maciejs-MacBook-Pro.local>
2023-03-30 10:11:45 +0200

    init

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+defineProps({
+  imageurl: String,
+  imgalt: String,
+  titleurl: String,
+  title: String,
+  description: String,
+  time: String,
+  servings: String,
+  level: String
+})
+
+</script>
+
+<template>
+  <div class="m-5 shadow-md w-80 rounded overflow-hidden    ">
+      <img :src="imageurl" :alt="imgalt">
+      <div class="p-2">
+        <div class="font-bold text-lg text-gray-700 leading-snug">
+          <a :href="titleurl" class="hover:underline">{{ title }}</a>
+        </div>
+        <p class="text-xs leading-tight tracking-wide my-2">{{ description }}</p>
+        <div class="flex pt-2 border-t border-gray-300 text-sm text-gray-700 divide-x-2">
+          <div class="flex-1 text-center">
+            <i class="far fa-clock"></i>
+            <p>{{ time }}</p>
+          </div>
+          <div class="flex-1 text-center">
+            <i class="fas fa-utensils"></i>
+            <p>{{ servings }}</p>
+          </div>
+          <div class="flex-1 text-center">
+            <i class="fas fa-signal"></i>
+            <p>{{ level }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</template>
+
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(max-aspect-ratio: 1/2)' },
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
+  },
+  plugins: [],
+}
+
+
